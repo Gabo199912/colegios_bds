5f4c7c8c43f36b9ad3fc71328bd551cfa9f0017e
Gabo <angelvasquez199912@icloud.com>
2026-09-10 18:28:31 -0600

    agregando cambios de bd

--- bd_colegios_querys.sql
+++ bd_colegios_querys.sql
@@ -1,6 +1,7 @@
 use bd_colegios;
 select * from usuario where activo = true;
 delete from usuario where id_usuario = 8;
+select * from usuario;
 
 select * from rol;
 
@@ -92,4 +93,69 @@ select  materia.id_materia,
 	inner join especialidad
 		on grado_academico.fk_id_especialidad = especialidad.id_especialidad
         order by id_materia;
+        
+        select * from grado_academico_materia;
+        
+select * from alumno 
+	inner join inscripcion 
+		on alumno.id_alumno = inscripcion.fk_id_alumno
+	inner join grado_academico 
+		on inscripcion.fk_id_grado_academico = grado_academico.id_grado_academico
+	inner join grado_academico_materia
+		on grado_academico.id_grado_academico = grado_academico_materia.fk_id_grado_academico;
+        
+        
+        
+        select * from materia 
+        inner join grado_academico_materia 
+			on materia.id_materia = grado_academico_materia.fk_id_materia
+		inner join grado_academico 
+			on grado_academico.id_grado_academico = grado_academico_materia.fk_id_grado_academico
+		inner join inscripcion 
+			on inscripcion.fk_id_grado_academico = grado_academico.id_grado_academico;
+            
+            
+SELECT 
+    a.id_alumno,
+    a.codigo_alumno,
+    CONCAT(u.nombre, ' ', u.apellido) AS nombre_alumno,
+    g.grado AS grado,
+    e.nombre_especialidad AS especialidad,
+    s.seccion AS seccion,
+    ce.anio AS ciclo_escolar,
+    m.nombre_materia AS materia
+FROM alumno a
+INNER JOIN usuario u 
+    ON u.id_usuario = a.fk_id_usuario
+INNER JOIN inscripcion i 
+    ON i.fk_id_alumno = a.id_alumno
+INNER JOIN grado_academico ga 
+    ON ga.id_grado_academico = i.fk_id_grado_academico
+INNER JOIN grado g 
+    ON g.id_grado = ga.fk_id_grado
+INNER JOIN especialidad e 
+    ON e.id_especialidad = ga.fk_id_especialidad
+INNER JOIN seccion s 
+    ON s.id_seccion = ga.fk_id_seccion
+INNER JOIN ciclo_escolar ce 
+    ON ce.id_ciclo_escolar = ga.fk_id_ciclo_escolar
+INNER JOIN grado_academico_materia gam 
+    ON gam.fk_id_grado_academico = ga.id_grado_academico
+INNER JOIN materia m 
+    ON m.id_materia = gam.fk_id_materia
+WHERE i.inscripcion_activa = TRUE
+ORDER BY 
+    a.id_alumno, 
+    m.nombre_materia;
+    
+    SELECT 
+    i.id_inscripcion,
+    i.fk_id_grado_academico AS ga_en_inscripcion,
+    i.inscripcion_activa,
+    ga.id_grado_academico AS ga_en_tabla,
+    ga.activo AS ga_activo
+FROM inscripcion i
+LEFT JOIN grado_academico ga 
+    ON ga.id_grado_academico = i.fk_id_grado_academico;
+
 
